e082f5b3dcdc05b23de8c1316207c23c2030c50c
girish <girish.budhwani@freecharge.com>
2016-05-29 15:05:41 +0530

    changed package

--- app/src/main/java/github/girish3/hackernews/interfaces/TopicView.java
+++
@@ -1,7 +0,0 @@
-package github.girish3.hackernews.interfaces;
-
-/**
- * Created by Girish on 28/05/16.
- */
-public class TopicView {
-}
---
+++ app/src/main/java/github/girish3/hackernews/views/TopicView.java
@@ -0,0 +1,22 @@
+package github.girish3.hackernews.views;
+
+import java.util.List;
+
+import github.girish3.hackernews.data.Topic;
+
+/**
+ * Created by Girish on 28/05/16.
+ */
+public interface TopicView {
+
+    void showTopics(List<Topic> topicList);
+
+    void showLoading();
+
+    void hideLoading();
+
+    void showError(String error);
+
+    void hideError();
+
+}
